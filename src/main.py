39f7bdd4d919e10d6ada4b97d0ede4a7dbb1b25c
Aaron Mamparo <aaronmamparo@gmail.com>
2021-04-21 23:16:24 -0500

    initial deployment

--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
+from typing import Any
+
 from flask import Flask
 from flask_cors import CORS
 from src.welcome_message import blueprint as welcome_message
 import awsgi
 
-# app = Flask(__name__)
-# CORS(app)
-# app.register_blueprint(welcome_message)
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(welcome_message)
 
 
-def lambda_handler(event, context):
-    print('hi')
-    print('event: ', event)
-    print('context: ', context)
-    return event
-    # return awsgi.response(app, event, context)
+def lambda_handler(event: dict, context: Any):
+    return awsgi.response(app, event, context)
 
-#
-# if __name__ == '__main__':
-#     app.run(port=8080, debug=True)
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
