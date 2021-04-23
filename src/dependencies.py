883100fa0b36e7a3c1585316401503debc16d90c
Aaron Mamparo <aaronmamparo@gmail.com>
2021-04-22 22:46:17 -0500

    bind environment

--- src/dependencies.py
+++ src/dependencies.py
@@ -2,6 +2,7 @@ from typing import Type, Callable
 
 from injector import Binder, singleton
 
+from src.environment import AbstractEnvironment, Environment
 from src.file_repository.abstract_file_repository import AbstractFileRepository
 from src.file_repository.local_file_repository import LocalFileRepository
 from src.file_repository.s3_file_repository import S3FileRepository
@@ -10,6 +11,7 @@ from src.file_repository.s3_file_repository import S3FileRepository
 def __compose(file_repository: Type[AbstractFileRepository]) -> Callable[[Binder], None]:
     def __configure(binder: Binder) -> None:
         binder.bind(AbstractFileRepository, to=file_repository, scope=singleton)
+        binder.bind(AbstractEnvironment, to=Environment, scope=singleton)
 
     return __configure
 
